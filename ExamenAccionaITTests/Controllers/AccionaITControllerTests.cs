1f06017b05617dd14e7745565f03b4a01a04ded8
fedetomadin <federico@tangol.com>
2020-04-23 23:21:34 -0300

    examen para Acciona IT

---
+++ ExamenAccionaITTests/Controllers/AccionaITControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExamenAccionaIT.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using ExamenAccionaIT.Models;
+
+namespace ExamenAccionaIT.Controllers.Tests
+{
+    [TestClass()]
+    public class AccionaITControllerTests
+    {
+        [TestMethod()]
+        public void NombreProvinciasTest()
+        {
+            AccionaITController metodosController = new AccionaITController();
+            string rta = metodosController.NombreProvincias();
+
+            var cantidadProvincias = JsonConvert.DeserializeObject<List<Provincias>>(rta);
+
+            Assert.AreEqual(cantidadProvincias.Count, 24);
+        }
+    }
+}
---
+++ TestUnitarios/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using ExamenAccionaIT.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExamenAccionaIT.Controllers;
+
+namespace TestUnitarios
+{
+    [TestClass]
+    public class TestsMetodos
+    {
+        [TestMethod]
+        public void ValidarUsuarioLoginTest()
+        {
+           Usuario user = new Usuario();
+
+           var login =  user.ValidarUsuarioLogin("admin", "123456");
+
+           Assert.AreEqual(login, true);
+        }
+
+
+        [TestMethod]
+        public void LoginTest()
+        {
+            Usuario user = new Usuario();
+            Persona per = new Persona();
+            string resultado = "";
+
+            if (user.ValidarUsuarioLogin("admin", "123456"))
+            {
+                resultado = "Se logueo bien";
+            }
+            else
+            {
+                resultado = "ERROR Loguin";
+            }
+
+            Assert.AreEqual(resultado, "Se logueo bien");
+        }
+    }
+}
